14c7861344129484706fd69b61c56b18427aabb1
Mahir Azmain <mahirazmain7@gmail.com>
2024-11-14 09:24:05 +0600

    Initial Commit

---
+++ FirebaseProjectTests/FirebaseProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseProjectTests.swift
+//  FirebaseProjectTests
+//
+//  Created by Mahir Azmain Haque on 14/11/24.
+//
+
+import Testing
+@testable import FirebaseProject
+
+struct FirebaseProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
