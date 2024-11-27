f6af9101a5282a893d13cb67f7ceb6f36460f0ba
Shayka Shipra <triviatruism@gmail.com>
2024-11-27 23:33:38 +0600

    FireStore Data Store Completed

---
+++ FirebaseProject/ListView.swift
@@ -0,0 +1,39 @@
+//
+//  ListView.swift
+//  FirebaseProject
+//
+//  Created by Mahir Azmain Haque on 27/11/24.
+//
+
+import SwiftUI
+struct ListView: View {
+    @EnvironmentObject var dataManager : DataManager
+    @State private var showPopup = false
+    
+    var body : some View {
+        NavigationView {
+            List( dataManager.dogs, id : \.id){ dog in
+                Text(dog.breed)
+                
+            }
+            .navigationTitle("Dogs")
+            .navigationBarItems(trailing: Button(action: {
+                showPopup.toggle()
+                
+            }, label: {
+                Image(systemName : "plus")
+            }))
+            .sheet(isPresented: $showPopup) {
+                NewDogView()
+            }
+                                                
+        }
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View{
+        ListView()
+            .environmentObject(DataManager())
+    }
+}
